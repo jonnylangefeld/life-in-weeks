82b9d0f7c6a3ff7f6daf37e2cc242ae98a787589
Jonny Lanefeld <jonny.langefeld@gmail.com>
2024-06-22 00:53:43 -0700

    slight improvements for mobile

--- src/components/year.tsx
+++ src/components/year.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Year({ year, data }: Props) {
   return (
     <div className="flex flex-row gap-3">
-      <div className="relative flex min-w-6 flex-grow-0">
+      <div className="relative flex min-w-3 flex-grow-0 lg:min-w-6">
         <div className="absolute bottom-0 right-0 flex h-full items-end text-[.8cqmax] leading-none sm:text-[.9cqmax] md:text-[1cqmax]">
           {year}
         </div>
