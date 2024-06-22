353f38329f1027a52b1b8e98c0dd4bd6cef37470
Jonny Lanefeld <jonny.langefeld@gmail.com>
2024-06-22 00:53:27 -0700

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Chart from "@/components/chart"
+import H1 from "@/components/ui/h1"
+
+export default function Home() {
+  return (
+    <>
+      <H1 className="whitespace-nowrap">My Life in Weeks</H1>
+      <Chart />
+    </>
+  )
+}
---
+++ src/components/year.tsx
@@ -0,0 +1,24 @@
+import { Data } from "./chart"
+import Week from "./week"
+
+interface Props {
+  year: number
+  data: Data
+}
+
+export default function Year({ year, data }: Props) {
+  return (
+    <div className="flex flex-row gap-3">
+      <div className="relative flex min-w-6 flex-grow-0">
+        <div className="absolute bottom-0 right-0 flex h-full items-end text-[.8cqmax] leading-none sm:text-[.9cqmax] md:text-[1cqmax]">
+          {year}
+        </div>
+      </div>
+      <div className="flex flex-grow flex-row contain-inline-size">
+        {Array.from({ length: 52 }).map((_, index) => (
+          <Week key={index} year={year} week={index + 1} data={data} />
+        ))}
+      </div>
+    </div>
+  )
+}
