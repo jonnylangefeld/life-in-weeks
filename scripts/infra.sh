4458befcc068b64a660a23f57bb71505ac923b98
Jonny Langefeld <jonnylangefeld@gmail.com>
2024-08-25 17:44:39 -0700

    update infra script

--- scripts/infra.sh
+++ scripts/infra.sh
@@ -10,5 +10,21 @@ gcloud config set project life-in-weeks-prod
 # * click 'Create Credentials'
 # * select 'OAuth client ID'
 # * select 'Web application'
-# configure supabase providers: https://supabase.com/dashboard/project/mbauxwpjmmlvbojestpa/auth/providers
+# * add these authorized JavaScript origins:
+#   * http://localhost
+#   * http://127.0.0.1
+#   * https://vercel.app
+#   * https://weeksin.life
+# * add these authorized redirect URIs:
+#   * http://127.0.0.1:54321/auth/v1/callback
+#   * http://localhost:54321/auth/v1/callback
+#   * https://wzmbtcpnjweqsqiubvii.supabase.co/auth/v1/callback
+#   * https://mbauxwpjmmlvbojestpa.supabase.co/auth/v1/callback
+
+# configure supabase providers with client id and secret from above: https://supabase.com/dashboard/project/mbauxwpjmmlvbojestpa/auth/providers
 # configure redirect urls: https://supabase.com/dashboard/project/mbauxwpjmmlvbojestpa/auth/url-configuration
+# for staging:
+# * https://*-jonny-langefelds-projects.vercel.app/**
+# for prod:
+# * https://life-in-weeks-xi.vercel.app/**
+# * https://weeksin.life/**
